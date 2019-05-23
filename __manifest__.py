730ba93150076d21a42b1b72dec9e8ea492b6e1a
Sergey Stepanets <stepanetssergey@gmail.com>
2019-05-23 15:59:54 +0300

    change new module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Module for test",
+    'version': '1.0',
+    'depends': ['base',
+                'web',
+                'sale',
+                'delivery',
+                'portal',
+                'account',
+                'mail',
+],
+    'author': "Sergey Stepanets",
+    'category': 'Application',
+    'description': """
+    Module MS
+    """,
+    'data': [
+    ],
+}
